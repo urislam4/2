1d63d3625d6c845248af702392a36028d1a4b986
urislam4 <isihohka@gmail.com>
2022-11-07 21:58:51 +0600

    app-js

--- loki.js
+++ loki.js
@@ -1,34 +1,27 @@
 
-console.log("client request...");
-console.log("string loading");
-const look = new Promise((resolve, rejekt)=>{
-    setTimeout (()=>{
-        const err = false;
+const block = document.querySelector('.block');
+const btn = document.querySelector('#button')
+console.log(block);
+console.log(btn);
 
-        if (!err){
-            const response={
-            userName:"user name",
-            userAge:"22",
-            hobbi:"play Gmae",
-            };
-            const json = JSON.stringify(err);
-            console.log("client get response from  server", json,response);
 
-            resolve(response);
-        }else{
-            rejekt("status code 404, not found");
-        }
-    },1000);
-}).then((res) =>{
-    setTimeout(()=>{
-        const data = {
-         userName:"user name",
-         userAge:"22",
-         hobbi:"play Gmae"
-        };
-       const Json = JSON.stringify(data);
-       console.log("client get data",Json,data,res);
-    },2000)
-    return"HELLO";
-})
-.then((data) =>alert(data))
+
+
+button.onclick = function() {
+    let start = Date.now();
+
+    let timer = setInterval(function() {
+      let time = Date.now() - start;
+
+      block.style.left = time / 5 + 'px';
+
+      if (time > 2000) clearInterval(timer);
+
+    }, 0,1);
+  }
+
+
+
+
+
+
